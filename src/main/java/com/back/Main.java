accc74450ce01fc278cce005fc6630ae6a9b545c
sanggyoon <sangreal99@naver.com>
2025-08-01 17:37:35 +0900

    6단계, 7단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -6,36 +6,46 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String order = "";
-        int idx = 0;
+        int idx = 1;
         WiseSaying[] ws = new WiseSaying[100];
 
         System.out.println("== 명언 ==");
-        while (!order.equals("종료")){
+        while (true) {
             System.out.print("명령) ");
-            order = sc.nextLine();
-            switch (order) {
-                case "등록":
-                    System.out.print("명언 : ");
-                    String content = sc.nextLine();
+            order = sc.nextLine().trim();
 
-                    System.out.print("작가 : ");
-                    String author = sc.nextLine();
+            if (order.equals("종료")) {
+                break;
+            } else if (order.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = sc.nextLine();
 
-                    ws[idx] = new WiseSaying();
-                    ws[idx].content = content;
-                    ws[idx].author = author;
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
 
-                    System.out.printf("%d번 명언이 등록되었습니다.\n", idx+1);
-                    idx++;
-                    break;
-                case "목록":
-                    System.out.println("------------------------------");
-                    for (int i=0; ws[i]!=null; i++){
-                        System.out.printf(i+1 + " / " + ws[i].author + " / " + ws[i].content + "\n");
+                ws[idx] = new WiseSaying();
+                ws[idx].content = content;
+                ws[idx].author = author;
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", idx);
+                idx++;
+            } else if (order.equals("목록")) {
+                System.out.println("------------------------------");
+                for (int i = 1; i < idx; i++) {
+                    if (ws[i] != null) {
+                        System.out.printf(i + " / " + ws[i].author + " / " + ws[i].content + "\n");
                     }
-                    break;
+                }
+            } else if (order.startsWith("삭제?id=")) {
+                String[] split = order.split("=");
+                int id = Integer.parseInt(split[1].trim());
+                if (id > 0 && id < idx && ws[id] != null) {
+                    ws[id] = null;
+                    System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
+                } else {
+                    System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+                }
             }
-
         }
     }
 }
