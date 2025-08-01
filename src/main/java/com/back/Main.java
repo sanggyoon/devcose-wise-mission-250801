6731a0250ae11b4b3604aed2778ca8ff641af3ff
sanggyoon <sangreal99@naver.com>
2025-08-01 17:45:39 +0900

    8단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -16,6 +16,7 @@ public class Main {
 
             if (order.equals("종료")) {
                 break;
+
             } else if (order.equals("등록")) {
                 System.out.print("명언 : ");
                 String content = sc.nextLine();
@@ -29,19 +30,42 @@ public class Main {
 
                 System.out.printf("%d번 명언이 등록되었습니다.\n", idx);
                 idx++;
+
             } else if (order.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
                 System.out.println("------------------------------");
                 for (int i = 1; i < idx; i++) {
                     if (ws[i] != null) {
                         System.out.printf(i + " / " + ws[i].author + " / " + ws[i].content + "\n");
                     }
                 }
+
             } else if (order.startsWith("삭제?id=")) {
                 String[] split = order.split("=");
                 int id = Integer.parseInt(split[1].trim());
                 if (id > 0 && id < idx && ws[id] != null) {
                     ws[id] = null;
                     System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
+                } else {
+                    System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+                }
+
+            } else if (order.startsWith("수정?id=")) {
+                String[] split = order.split("=");
+                int id = Integer.parseInt(split[1].trim());
+                if (id > 0 && id < idx && ws[id] != null) {
+                    System.out.printf("명언(기존) : %s\n", ws[id].content);
+                    System.out.print("명언 : ");
+                    String newContent = sc.nextLine();
+
+                    System.out.printf("작가(기존) : %s\n", ws[id].author);
+                    System.out.print("작가 : ");
+                    String newAuthor = sc.nextLine();
+
+                    ws[id].content = newContent;
+                    ws[id].author = newAuthor;
+
+                    System.out.printf("%d번 명언이 수정되었습니다.\n", id);
                 } else {
                     System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
                 }
