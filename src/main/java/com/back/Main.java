b9252e3a6037d2e27a63d25c80fe3e4a37f345e6
sanggyoon <sangreal99@naver.com>
2025-08-01 15:53:12 +0900

    4단계, 5단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -17,16 +17,22 @@ public class Main {
                 case "등록":
                     System.out.print("명언 : ");
                     String content = sc.nextLine();
-                    ws[idx].content = content;
 
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
+
+                    ws[idx] = new WiseSaying();
+                    ws[idx].content = content;
                     ws[idx].author = author;
 
-                    System.out.printf("%d번 명언이 등록되었습니다.", idx+1);
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", idx+1);
                     idx++;
                     break;
                 case "목록":
+                    System.out.println("------------------------------");
+                    for (int i=0; ws[i]!=null; i++){
+                        System.out.printf(i+1 + " / " + ws[i].author + " / " + ws[i].content + "\n");
+                    }
                     break;
             }
 
